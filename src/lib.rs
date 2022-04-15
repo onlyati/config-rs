b183df3af843f86fddf31f0a07dfeb1570fc26fa
Attila Molnar <molnar.attila28@gmail.com>
2022-04-15 14:31:16 +0200

    include function test

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,37 @@ pub fn read_config(config_path: &str) -> Result<HashMap<String, String>, String>
                 point = &v[..];
             }
 
+            // Handle special commands
+            if point.len() > 8 {
+                if &point[0..7] == "%include" {
+                    let mut alt_lines = point.split_whitespace();
+                    let alt_line = alt_lines.next();
+                    let alt_line = alt_lines.next();
+                    if let Some(l) = alt_line {
+                        if &l[0..1] == "/" {
+                            // Absolute paht
+                            let alt_config = read_config(l);
+                            match alt_config {
+                                Ok(r) => {
+                                    for (key, value) in r {
+                                        config.insert(key, value);
+                                    }
+                                },
+                                Err(e) => return Err(e),
+                            }
+                        }
+                        else
+                        {
+                            // Relative path
+                            let alt_config = read_config(format!("{}/{}", config_path, l).as_str());
+                        }
+                    }
+                    else {
+                        return Err(String::from("No file specified after %include statement"));
+                    }
+                }
+            }
+
             // Process the pure data
             if let Some(index) = point.find("=") {
                 let property = String::from(&point[0..index]).trim().to_string();
